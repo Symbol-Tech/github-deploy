7ce9a0f19c9731b0d9ff1d6934e3ce34557dc204
Nádas Tamás <nadas.tamas@symboltech.hu>
2023-04-17 15:34:43 +0200

    Check empty request

--- index.php
+++ index.php
@@ -10,38 +10,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $rawdata = file_get_contents("php://input");
   $decoded = json_decode($rawdata);
 
-  $logfile = "./log/{$time}.log";
-  file_put_contents($logfile, print_r($decoded, true));
+  if ($decoded) {
+    $logfile = "./log/{$time}.log";
+    file_put_contents($logfile, print_r($decoded, true));
 
-  $result = new stdClass();
+    $result = new stdClass();
 
-  $start = microtime(true);
-  $repo = $decoded->repository->full_name;
-  $branch = str_replace('refs/heads/', '', $decoded->ref);
-  $commitmsgs = [];
-  foreach ($decoded->commits as $commit)
-    $commitmsgs[] = $commit->message;
-  sort($commitmsgs);
+    $start = microtime(true);
+    $repo = $decoded->repository->full_name;
+    $branch = str_replace('refs/heads/', '', $decoded->ref);
+    $commitmsgs = [];
+    foreach ($decoded->commits as $commit)
+      $commitmsgs[] = $commit->message;
+    sort($commitmsgs);
 
-  $result->repo = $repo;
-  $result->branch = $branch;
-  $result->commits = $commitmsgs;
+    $result->repo = $repo;
+    $result->branch = $branch;
+    $result->commits = $commitmsgs;
 
-  if ($exec = $targets[$repo][$branch]) {
-    $result->exec = $exec;
-    exec($exec, $execresult);
-    if ($execresult)
-      $result->execresult = $execresult;
+    if ($exec = $targets[$repo][$branch]) {
+      $result->exec = $exec;
+      exec($exec, $execresult);
+      if ($execresult)
+        $result->execresult = $execresult;
+    }
+
+    $time_elapsed_secs = round(1000 * (microtime(true) - $start));
+    $result->status = "OK";
+    $result->elapsed = $time_elapsed_secs;
+
+    echo json_encode($result);
+
+    if (defined('ALERT_EMAIL'))
+      mail(ALERT_EMAIL, "Deploy - $repo/$branch", json_encode($result, JSON_PRETTY_PRINT));
+  } else {
+    $logfile = "./log/{$time}.log";
+    file_put_contents($logfile, print_r("Empty request", true));
   }
-
-  $time_elapsed_secs = round(1000 * (microtime(true) - $start));
-  $result->status = "OK";
-  $result->elapsed = $time_elapsed_secs;
-
-  echo json_encode($result);
-
-  if (defined('ALERT_EMAIL'))
-    mail(ALERT_EMAIL, "Deploy - $repo/$branch", json_encode($result, JSON_PRETTY_PRINT));
 } else {
   echo "404 - Page not found";
 }
